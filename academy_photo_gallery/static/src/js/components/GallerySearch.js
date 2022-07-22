6bebdbfd795eb63a1011d8f44d545770670010a8
LuisAngelGonzalezC <luis.gonzalez@quadit.mx>
2022-07-22 10:16:07 -0500

    [REM] academy_photo_gallery: translation of placeholder in search

--- academy_photo_gallery/static/src/js/components/GallerySearch.js
+++ academy_photo_gallery/static/src/js/components/GallerySearch.js
@@ -1,6 +1,5 @@
 /** @odoo-module alias=academy_photo_gallery.gallery_search **/
 
-import { _t } from 'web.core';
 const { Component } = owl;
 const { useRef } = owl.hooks;
 const { xml } = owl.tags;
@@ -9,7 +8,7 @@ class GallerySearch extends Component {
     static template = xml`
     <t t-name="GallerySearch" owl="1">
         <div class="form-group">
-            <input t-ref="search-word-input" t-on-keyup="updateSearch" type="text" class="form-control" placeholder="${_t('Search')}"/>
+            <input t-ref="search-word-input" t-on-keyup="updateSearch" type="text" class="form-control" placeholder="Search"/>
         </div>
     </t>`;
     setup() {
